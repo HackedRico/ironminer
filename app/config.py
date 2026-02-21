10349aac95b5dfaa5b0958df86669a8692b0efad
HackedRico <chenricky19@gmail.com>
2026-02-21 14:39:59 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -11,3 +11,6 @@ VESTAI_BASE_URL = os.getenv("VESTAI_BASE_URL", "")
 LLM_PROVIDER = os.getenv("LLM_PROVIDER", "ollama")
 OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3.2")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
